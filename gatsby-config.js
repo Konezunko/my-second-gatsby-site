c730a1e050eadfed7dc31ab69a7f876132f582b5
Konezunko <21025167@mmm.muroran-it.ac.jp>
2023-10-25 01:03:32 +0900

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,9 @@
 module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
+    description: `Example project for the Gatsby Head API`,
+    twitterUsername: `@gatsbyjs`,
+    image: `/gatsby-icon.png`,
   },
   plugins: [],
 }
